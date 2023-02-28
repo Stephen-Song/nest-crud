eff9628982510df094d89500083d94c13914cadf
xiaosong <duochizhacai@gmail.com>
2023-02-28 10:25:27 +0800

    feat: add database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import chalk from 'chalk';
+import { env } from 'config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
+console.log(
+  `${chalk.bgBlueBright(' Ready to connect ')} 当前环境为 ${chalk.redBright(
+    process.env.NODE_ENV,
+  )}`,
+);
+
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(env.DATABASE_CONFIG), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
